36e1d2b6734fef38db196b78fbf79101fffa8282
Konanxd <iyasaggni@gmail.com>
2025-06-03 15:29:32 +0700

    Fix migrate error by renaming organization migration files

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Organization extends Model
 {
--- database/migrations/2025_05_02_113040_create_organizations_table.php
+++ database/migrations/2025_05_02_113040_create_organizations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
-            $table->name();
+            $table->string('name');
             $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
